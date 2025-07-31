a79831199fcdd1554e3338cb5be9db5478539d3e
xlly-s2 <156248534+xlly-s2@users.noreply.github.com>
2025-07-31 20:42:55 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Use getenv() for Railway compatibility
-define('TMDB_API_KEY', getenv('1fc6331e4d92af4f02ffef00c494137f'));
+define('TMDB_API_KEY', '1fc6331e4d92af4f02ffef00c494137f'); // Make sure this is set
 define('TMDB_BASE_URL', 'https://api.themoviedb.org/3/');
 define('TMDB_IMAGE_URL', 'https://image.tmdb.org/t/p/');
 ?>
